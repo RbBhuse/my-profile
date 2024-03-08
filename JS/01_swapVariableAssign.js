b57cc71f54ff677d168e1fa7929f213ac7d6d380
RbBhuse <reshmabhuse7@gmail.com>
2024-03-08 20:02:09 +0530

    Program on print variable value and swap value of variable using third variable
    Program on print variable value and swap value of variable using third variable

---
+++ JS/01_swapVariableAssign.js
@@ -0,0 +1,29 @@
+console.log("======= Assignment no2 ========");
+console.log("=======step no1 ========");
+var sweety = "Sweety";
+var cutie = "Cutie";
+var temp;
+console.log("======== Before Swap ========");
+console.log("Sweety:", sweety, "Cutie:", cutie);
+
+console.log("======== after Swap ========");
+temp = sweety;
+sweety = cutie;
+cutie = temp;
+console.log("Sweety:", sweety, "Cutie:", cutie);
+
+console.log("=======step no2 ========");
+var num1 = 100;
+var num2 = 200;
+var num3 = 300;
+var temp;
+console.log("======== Before Swap ========");
+console.log("num1:", num1, "num2:", num2, "num3:", num3);
+
+console.log("======== after Swap ========");
+
+temp = num1;
+num1 = num2;
+num2 = num3;
+num3 = temp;
+console.log("num1:", num1, "num2:", num2, "num3:", num3);
---
+++ JS/01_variableAssign.js
@@ -0,0 +1,33 @@
+console.log("======= Assignment no1 ========");
+console.log("=======step no1 ========");
+var myFullName;
+console.log("Before Initialization:", myFullName);
+myFullName = "Reshma Bhuse";
+console.log("My Name is:", myFullName);
+myFullName = "Reshma Prabhakar Bhuse";
+console.log("Updated value of variable is:", myFullName);
+console.log("=======step no2 ========");
+var myLove;
+myLove = "I Love only Js";
+console.log("My Love is:", myLove);
+console.log("=======step no3 ========");
+var firstName = "Reshma";
+console.log("First Name is:", firstName);
+
+var lastName = "Bhuse";
+console.log("Last Name is:", lastName);
+
+var age = 24;
+console.log("Age is:", age);
+
+var address = "Hinjewadi Pune";
+console.log("Address is:", address);
+
+var pinCode = 1234;
+console.log("PinCode is:", pinCode);
+
+var gender = "Female";
+console.log("Gender is:", gender);
+
+var isMarried = true;
+console.log("Marital status:", isMarried);
