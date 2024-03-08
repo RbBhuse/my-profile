cff5ef6373b469a4688d8c7d5834bb70f8d5eaba
RbBhuse <reshmabhuse7@gmail.com>
2024-03-08 19:59:05 +0530

    Simple Program to to Print value in JS
    Simple Program to to Print value in JS

---
+++ JS/helloWorld.js
@@ -0,0 +1,13 @@
+// Camel Case: test.js, hello.js, helloWorld.js, myFirstProgram.js
+
+// Kebab: test.js, hello.js, hello-world.js, my-first-program.js
+
+// Snake: test.js, hello.js, hello_world.js, my_first_program.js
+
+var city; // Variable Declaration
+
+city="Pune";  // Variable Initialization
+
+console.log(city);
+
+console.log("My city name is:", city);
