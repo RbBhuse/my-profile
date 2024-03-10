37de160163648d3457e58c6242072c8a9b3bb36f
RbBhuse <reshmabhuse7@gmail.com>
2024-03-10 15:07:26 +0530

    Handson on typeOf operator
    Handson on typeOf operator

---
+++ JS/02_typeOf.js
@@ -0,0 +1,39 @@
+console.log('======= Assignment no1 ========');
+console.log('=======step no1 ========');
+var bankName;
+var typeOfBankName=typeof bankName;
+console.log('Before Initialization:');
+console.log('Value of Bank Name:',bankName,',' ,'Type of Bank Name:',typeOfBankName);
+bankName='SBI Bank';
+var typeOfBankName=typeof bankName;
+console.log('After Initialization:');
+console.log('Value of Bank Name:',bankName,',' ,'Type of Bank Name:',typeOfBankName);
+
+
+console.log('=======step no2 ========');
+var  marks = '90%';
+var typeofMarks=typeof marks;
+console.log('Value of Marks:',marks,',' ,'Type of marks:',typeofMarks);
+
+console.log('=======step no3 ========');
+var isWorking;
+console.log('Before Initialization:');
+var typeOfisWorking= typeof isWorking;
+console.log('Value of is Working:',isWorking,',' ,'Type of isWorking:',typeOfisWorking);
+console.log('After Initialization:');
+isWorking=true;
+var typeOfisWorking= typeof isWorking;
+console.log('Value of is Working:',isWorking,',' ,'Type of isWorking:',typeOfisWorking);
+
+
+console.log('=======step no4 ========');
+console.log('Before update value :')
+var totalCount= "Hundred and Seven";
+var typeOFCount= typeof totalCount;
+console.log('Value of Total Count:',totalCount,',','Type of Count:', typeOFCount);
+
+var totalCount=107;
+console.log('After update value :');
+var typeOFCount= typeof totalCount;
+console.log('Value of Total Count:',totalCount,',','Type of Count:', typeOFCount);
+
