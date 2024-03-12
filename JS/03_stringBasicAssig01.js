a05f1b801b7d03e456ded060df43e9784e519c9a
RbBhuse <reshmabhuse7@gmail.com>
2024-03-12 18:06:08 +0530

    Hands-on on String Template and string length property
    Hands-on on String Template and string length property

---
+++ JS/03_stringBasicAssig01.js
@@ -0,0 +1,18 @@
+console.log(`======Assignment no1======`);
+function stringBasic() {
+  console.log(`=======Step1 =======`);
+  console.log(`My dream company is "TCS"`);
+
+  console.log(`=======Step3 =======`);
+  var hobby1 = "Singing";
+  var hobby2 = "cycling";
+  var hobby3 = "Drawing";
+  console.log(`My Hobbies are: ${hobby1},${hobby2},${hobby3}`);
+
+  var len1 = hobby1.length;
+  var len2 = hobby2.length;
+  var len3 = hobby3.length;
+  var totalChar = len1 + len2 + len3;
+  console.log(`Total character available in all hobbies are: ${totalChar}`);
+}
+stringBasic();
