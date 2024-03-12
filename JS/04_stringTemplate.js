b8eac00f22df79932faae27dbd6571a76c9fb1a5
RbBhuse <reshmabhuse7@gmail.com>
2024-03-12 17:54:03 +0530

    String Template
    String Template

---
+++ JS/04_stringTemplate.js
@@ -0,0 +1,13 @@
+console.log(`I am "Software" Developer`);
+
+ 
+
+var city = "Pune";
+
+var firstName = "Gajanan";
+//console.log("My name is: ", firstName, " and city is: ", city);
+console.log(` My name is: ${firstName} and city is: ${city} `);
+console.log(`=================`);
+//another way to write string template
+ var stringFormat= ` My name is: ${firstName} and city is: ${city} `;
+ console.log(stringFormat);
