2f20330a180c9acc3237007cb8b91d169f6b99fd
RbBhuse <reshmabhuse7@gmail.com>
2024-03-17 11:38:44 +0530

    Program on PostFix operator
    Program on PostFix operator

---
+++ JS/05_postFixOperator.js
@@ -0,0 +1,17 @@
+var a=5;
+var b = a++;
+
+console.log(`value of "a" is: ${a}`);
+console.log(`value of "b" is: ${b}`);
+
+var n1=5;
+
+var res= n1++ + ++n1 +n1++;
+console.log(`Value of result: ${res}`);
+console.log(`Value of n1 is: ${n1}`);
+
+var c= 2;
+
+var d= ++c + ++c + c++ - --c
+console.log(`Value of "d" is: ${d}`);
+console.log(`Value of "c" is: ${c}`);
